4b11350364d85a9487e26df6b8e045ca86399e20
skyadav456 <sharadk4545@gmail.com>
2025-06-11 02:30:47 +0530

    Initial Commit

---
+++ Mini-Proj-01-User-Mgmt-App/src/main/java/com/sharad/MiniProj01UserMgmtAppApplication.java
@@ -0,0 +1,13 @@
+package com.sharad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProj01UserMgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProj01UserMgmtAppApplication.class, args);
+	}
+
+}
